36df1c6524edc181d70a7e78d14a8d09611a9f5f
Kenny Wong <kennyviperhk@gmail.com>
2019-11-26 12:37:08 +0800

    on progress

--- src/main/java/rita/Tagger.java
+++ src/main/java/rita/Tagger.java
@@ -11,7 +11,7 @@ public class Tagger
 	
   public boolean isAdjective(String word)
   {
-	    return this.checkType(word, ADJ);
+	    return checkType(word, ADJ);
   }
 
   public boolean isAdverb(String word)
@@ -44,4 +44,25 @@ public class Tagger
     return null;
   }
 
+  private boolean checkType(String word, String[] tagArray) {
+
+	    if (word) {
+
+	      if (!word.length) return false;
+
+	      if (word.indexOf(' ') < 0) {
+
+	        let psa = RiTa._lexicon()._posArr(word);
+
+	        if (RiTa.LEX_WARN && psa.length < 1 && this.size() <= 1000) {
+	          warn(RiTa.LEX_WARN);
+	          RiTa.LEX_WARN = 0; // only once
+	        }
+
+	        return psa.filter(p => tagArray.indexOf(p) > -1).length > 0;
+	      }
+
+	      throw Error("checkType() expects single word, found: '" + word + "'");
+	    }
+	  }
 }
