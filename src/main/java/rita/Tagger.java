2e2f6a124af7b946efa1158ba0aeda85d015c1b6
Kenny Wong <kennyviperhk@gmail.com>
2019-12-23 14:26:21 +0800

    update

--- src/main/java/rita/Tagger.java
+++ src/main/java/rita/Tagger.java
@@ -6,6 +6,8 @@ import java.util.HashSet;
 import java.util.List;
 
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.sun.xml.internal.ws.util.StringUtils;
 
@@ -16,6 +18,8 @@ public class Tagger
 	public static final String[] ADV = {"rb", "rbr", "rbs", "rp"};
 	public static final String[] NOUNS = {"nn", "nns", "nnp", "nnps"};
 	public static final String[] VERBS = {"vb", "vbd", "vbg", "vbn", "vbp", "vbz"};
+	
+	static String[] MODALS = Util.MODALS;
 
 
 	public static boolean isAdjective(String word)
@@ -182,7 +186,7 @@ public class Tagger
 	    }
 
 	    // Adjust pos according to transformation rules
-	    String[] tags = _applyContext(words, result, choices2d);
+	    String[] tags = _applyContext(words, result.toArray(new String[result.size()]), choices2d.toArray(new String[choices2d.size()]));
 	    System.out.println("choices2d : " + choices2d);
 	    if (useSimpleTags) {
 	      for (int i = 0; i < tags.length; i++) {
@@ -197,10 +201,163 @@ public class Tagger
 	    return ((tags == null) ? new String[] {} : tags);
 	}
 
-	private static String[] _applyContext(String words, List<String> result, List<String> choices2d) {
-		// TODO Auto-generated method stub
+	private static String[] _applyContext(String words, String[] result, String[] choices2d) {
+
+		//console.log("ac(" + words + "," + result + "," + choices2d + ")");
+
+	    // Apply transformations
+	    for (int i = 0, l = words.length(); i < l; i++) {
+
+	      String word = Character.toString(words.charAt(i));
+	      String tag = result[i];
+	      String[] resultSA = result;
+	      // transform 1a: DT, {VBD | VBP | VB} --> DT, NN
+	      if (i > 0 && (resultSA[i - 1] == "dt")) {
+
+	        if (tag.startsWith("vb")) {
+	          tag = "nn";
+
+	          // transform 7: if a word has been categorized as a
+	          // common noun and it ends with "s", then set its type to plural common noun (NNS)
+
+
+	          if (word.matches("^.*[^s]s$")) {
+	            if (!Arrays.asList(MODALS).contains(word)) {
+	              tag = "nns";
+	            }
+	          }
+
+	          _logCustom("1a", word, tag);
+	        }
+
+	        // transform 1b: DT, {RB | RBR | RBS} --> DT, {JJ |
+	        // JJR | JJS}
+	        else if (tag.startsWith("rb")) {
+
+	          tag = (tag.length() > 2) ? "jj" + tag.charAt(2) : "jj";
+	          _logCustom("1b", word, tag);
+	        }
+	      }
+
+	      // transform 2: convert a noun to a number (cd) if it is
+	      // all digits and/or a decimal "."
+	      if (tag.startsWith("n") && choices2d[i].length() != 0 ) {
+	        if (isNumeric(word)) {
+	          tag = "cd";
+	        } // mods: dch (add choice check above) <---- ? >
+	      }
+
+	      // transform 3: convert a noun to a past participle if
+	      // word ends with "ed" and (following any nn or prp?)
+	      if (i > 0 && tag.startsWith("n") && word.endsWith("ed") && !word.endsWith("eed") && resultSA[i - 1].matches("^(nn|prp)$")) {
+	        tag = "vbn";
+	      }
+
+	      // transform 4: convert any type to adverb if it ends in "ly";
+	      if (word.endsWith("ly")) {
+	        tag = "rb";
+	      }
+
+	      // transform 5: convert a common noun (NN or NNS) to a
+	      // adjective if it ends with "al", special-case for mammal
+	      if (tag.startsWith("nn") && word.endsWith("al") && word != "mammal") {
+	        tag = "jj";
+	      }
+
+	      // transform 6: convert a noun to a verb if the
+	      // preceeding word is modal
+	      if (i > 0 && tag.startsWith("nn") && resultSA[i - 1].startsWith("md")) {
+	        tag = "vb";
+	      }
+
+	      // transform 8: convert a common noun to a present
+	      // participle verb (i.e., a gerund)
+	      if (tag.startsWith("nn") && word.endsWith("ing")) {
+
+	        // DH: fixed here -- add check on choices2d for any verb: eg. // "morning"
+	        if (hasTag(choices2d[i], "vb")) {
+	          tag = "vbg";
+	          _logCustom(8, word, tag);
+	        }
+	      }
+
+	      // transform 9(dch): convert plural nouns (which are also 3sg-verbs) to
+	      // 3sg-verbs when following a singular noun (the dog dances, Dave dances, he dances)
+	      if (i > 0 && tag == "nns" && hasTag(choices2d[i], "vbz") && resultSA[i - 1].match(/^(nn|prp|nnp)$/)) {
+	        tag = "vbz";
+	        _logCustom(9, word, tag);
+	      }
+
+	      // transform 10(dch): convert common nouns to proper
+	      // nouns when they start w' a capital
+	      if (tag.startsWith("nn") && (word.charAt(0) == word.charAt(0).toUpperCase())) {
+	        //if it is not at the start of a sentence or it is the only word
+	        // or when it is at the start of a sentence but can't be found in the dictionary
+	        if (i != 0 || words.length == 1 || (i == 0 && !_lexHas("nn", RiTa.singularize(word).toLowerCase()))) {
+	          tag = tag.endsWith("s") ? "nnps" : "nnp";
+	          _logCustom(10, word, tag);
+	        }
+	      }
+
+	      // transform 11(dch): convert plural nouns (which are
+	      // also 3sg-verbs) to 3sg-verbs when followed by adverb
+	      if (i < result.length - 1 && tag == "nns" && resultSA[i + 1].startsWith("rb") &&
+	        hasTag(choices2d[i], "vbz")) {
+	        tag = "vbz";
+	        _logCustom(11, word, tag);
+	      }
+
+	      // transform 12(dch): convert plural nouns which have an entry for their base form to vbz
+	      if (tag == "nns") {
+
+	        // is preceded by one of the following
+	        if (i > 0 && ["nn", "prp", "cc", "nnp"].indexOf(resultSA[i - 1]) > -1) {
+	          // if word is ends with s or es and is "nns" and has a vb
+	          if (_lexHas("vb", RiTa.singularize(word))) {
+	            tag = "vbz";
+	            _logCustom(12, word, tag);
+	          }
+	        } // if only word and not in lexicon
+	        else if (words.length == 1 && !choices2d[i].length) {
+	          // if the stem of a single word could be both nn and vb, return nns
+	          // only return vbz when the stem is vb but not nn
+	          if (!_lexHas("nn", RiTa.singularize(word)) && _lexHas("vb", RiTa.singularize(word))) {
+	            tag = "vbz";
+	            _logCustom(12, word, tag);
+	          }
+
+	        }
+	      }
+
+	      //transform 13(cqx): convert a vb/ potential vb to vbp when following nns (Elephants dance, they dance)
+	      if (tag == "vb" || (tag == "nn" && hasTag(choices2d[i], "vb"))) {
+	        if (i > 0 && resultSA[i - 1].match("^(nns|nnps|prp)$")) {
+	          tag = "vbp";
+	          _logCustom(13, word, tag);
+	        }
+	      }
+
+	      resultSA[i] = tag;
+	    }
+
+	    return result;
+		
+		
 		return null;
 	}
+
+	private static void _logCustom(String i, String frm, String to) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+	private static boolean hasTag(String[] choices, String tag) {
+	  //  if (!Array.isArray(choices)) return false;
+	    String choiceStr = String.join("", choices);
+	    return (choiceStr.indexOf(tag) > -1);
+	  }
+
 
 	private static boolean _lexHas(String string, String sub) {
 		// TODO Auto-generated method stub
@@ -243,7 +400,7 @@ public class Tagger
 			
 			
 			if(psa.size() == 0) {
-				if (RiTa.LEX_WARN) { // TODO what is this.size() <= 1000 ??
+				if (RiTa.LEX_WARN) { // TODO what is size() <= 1000 ??
 					Logger logger = Logger.getLogger( Tagger.class.getName()); 
 					logger.warning(Boolean.toString(RiTa.LEX_WARN));
 					RiTa.LEX_WARN = false; // only once
