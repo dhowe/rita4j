a9d21d08d0c04df406bc987c1bc25b3c42c86e39
dhowe <dhowe@cims.nyu.edu>
2020-09-14 22:49:00 +0800

    initial

---
+++ src/test/java/rita/test/KnownIssues.java
@@ -0,0 +1,13 @@
+package rita.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import rita.RiTa;
+
+public class KnownIssues {
+
+	// Failing tests we need to work on go here
+
+}
